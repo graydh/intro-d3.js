de79c7fd2981b3501184c05fbce017b546f09a4a
Declan Gray-Mullen <dgraymullen@umass.edu>
2022-02-28 14:14:12 -0500

    axis example

--- script.js
+++ script.js
@@ -1,47 +1,33 @@
-const xValue = d => d.Horsepower;
-const yValue = d => d.Displacement;
-const margin = { left: 30, right: 20, top: 20, bottom: 20 };
+var data = [3, 5, 15, 25, 50]
 
-const svg = d3.select('svg');
-const width = svg.attr('width');
-const height = svg.attr('height');
-const innerWidth = width - margin.left - margin.right;
-const innerHeight = height - margin.top - margin.bottom;
+var wh = 400;
+var padding = 10;
 
-const g = svg.append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
-const xAxisG = g.append('g')
-    .attr('transform', `translate(0, ${innerHeight})`);
-const yAxisG = g.append('g');
 
-const xScale = d3.scaleLinear();
-const yScale = d3.scaleLinear();
+var svg = d3.select('svg');
+var chart = svg.append('g')
+    .attr('class', 'chart')
+    .attr('transform', 'translate(' + padding + ',' + (wh - padding) + ')');
 
-const xAxis = d3.axisBottom().scale(xScale);
-const yAxis = d3.axisLeft().scale(yScale);
+var xScale = d3.scaleLinear()
+    .domain([0,60])
+    .range([0,wh]);
 
-const row = d => {
-  d.Horsepower = +d.Horsepower;
-  d.Displacement = +d.Displacement;
-  return d;
-};
+var xAxis = d3.axisBottom()
+  .scale(xScale)
+  .tickValues([0,5,10,60]);
 
-d3.csv('cars.csv', row)
-  .then(data => {
-    xScale
-      .domain(d3.extent(data, xValue))
-      .range([0, innerWidth]);
+var axis = chart.append('g')
+    .attr('class', 'axis')
+    .call(xAxis);
 
-    yScale
-      .domain(d3.extent(data, yValue))
-      .range([innerHeight, 0]);
+var rectangles = chart.append('g')
+    .attr('class', 'symbols');
 
-    g.selectAll('circle').data(data)
-      .enter().append('circle')
-      .attr('cx', d => xScale(xValue(d)))
-      .attr('cy', d => yScale(yValue(d)))
-      .attr('r', 5);
-
-    xAxisG.call(xAxis);
-    yAxisG.call(yAxis);
-  });
+rectangles.selectAll('rect')
+  .data(data)
+  .join('rect')
+  .attr('x', 0)
+  .attr('y', function(d,i){return -(wh/data.length)*(i+1)})
+  .attr('width', function(d){return xScale(d)})
+  .attr('height', 10)

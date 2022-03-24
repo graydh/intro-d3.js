e1d251c4e2b92c9130805d53515dd298ad425312
Declan Gray-Mullen <>
2022-03-24 18:13:45 +0000

    add population-density color

--- script.js
+++ script.js
@@ -1,24 +1,52 @@
 // https://gist.github.com/d3noob/82f4db23d47971c74699abb5f4bf8204
-var width = 960,
-    height = 500;
 
-var projection = d3.geoMercator();
+// load population data into dict
+var pops = d3.json("pops.json").then(
+  function(data){
+    var country_data = {};
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    for (var i = 0, emp; i < data.length; i++) {
+       emp = data[i];
+       country_data[ emp.ccn3 ] = emp;
+       console.log(emp.Density)
+    }
 
-var path = d3.geoPath()
-    .projection(projection);
+    var color = d3.scaleSequential(d3.interpolate("white", "red"))
+      .domain([0,2000]);
 
-var g = svg.append("g");
+  var width = 960,
+      height = 500;
+  
+  var projection = d3.geoMercator();
+  
+  var svg = d3.select("body").append("svg")
+      .attr("width", width)
+      .attr("height", height);
+  
+  var path = d3.geoPath()
+      .projection(projection);
+  
+  var g = svg.append("g");
+  
+  // load and display the World
+  d3.json("world-110m2.json").then(function(topology) {
+  
+      g.selectAll("path")
+         .data(topojson.feature(topology, topology.objects.countries).features)
+         .enter().append("path")
+         .attr("d", path)
+         .attr("fill", function(d){
+           if(d.id in country_data){
+             if(country_data[d.id].hasOwnProperty('Density')){
+               return color(country_data[d.id].Density);
+             }
+           }
+           return 'white'
+         });
+  });
+  }
+);
 
-// load and display the World
-d3.json("world-110m2.json").then(function(topology) {
 
-    g.selectAll("path")
-       .data(topojson.feature(topology, topology.objects.countries).features)
-       .enter().append("path")
-       .attr("d", path);
 
-});
+

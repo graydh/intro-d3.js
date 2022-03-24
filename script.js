0bc29275eedd0ae45b51c63668bb8b370ad6e0e4
Declan Gray-Mullen <dgraymullen@umass.edu>
2022-03-24 01:35:16 +0000

    added basic scaleBand example

--- script.js
+++ script.js
@@ -1,68 +1,28 @@
-// http://bl.ocks.org/alansmithy/e984477a741bc56db5a5
+var myData = [
+	{day : 'Mon', value: 10},
+	{day : 'Tue', value: 40},
+	{day : 'Wed', value: 30},
+	{day : 'Thu', value: 60},
+	{day : 'Fri', value: 30}
+];
 
-const svg = d3.select('svg');
-//2 different data arrays
-var dataArray1 = [30,35,45,55,70];
-var dataArray2 = [50,55,45,35,20,25,25,40];
+var height = 100;
 
-//globals
-var dataIndex=1;
-var xBuffer=50;
-var yBuffer=150;
-var lineLength=400;
+var bandScale = d3.scaleBand()
+	.domain(['Mon', 'Tue', 'Wed', 'Thu', 'Fri'])
+	.range([0, 300])
+	.paddingInner(0.05);
 
-
-svg.append("text")
-    .attr("x",xBuffer+(lineLength/2))
-    .attr("y",50)
-    .text("dataset"+dataIndex);
-
-//create axis line
-svg.append("line")
-    .attr("x1",xBuffer)
-    .attr("y1",yBuffer)
-    .attr("x1",xBuffer+lineLength)
-    .attr("y2",yBuffer)
-
-//create basic circles
-svg.append("g").selectAll("circle")
-    .data(eval("dataArray"+dataIndex))
-    .enter()
-    .append("circle")
-    .attr("cx",function(d,i){
-        var spacing = lineLength/(eval("dataArray"+dataIndex).length);
-        return xBuffer+(i*spacing)
-    })
-    .attr("cy",yBuffer)
-    .attr("r",function(d,i){return d});
-
-//button to swap over datasets
-d3.select("body").append("button")
-    .text("change data")
-    .on("click",function(){
-        //select new data
-        if (dataIndex==1) {
-            dataIndex=2;
-        } else   {
-            dataIndex=1;
-        }
-        //rejoin data
-        var circle = svg.select("g").selectAll("circle")
-            .data(eval("dataArray"+dataIndex));
-
-        circle.exit().remove();//remove unneeded circles
-        circle.enter().append("circle")
-            .attr("r",0);//create any new circles needed
-
-        //update all circles to new positions
-        circle.transition()
-            .duration(500)
-            .attr("cx",function(d,i){
-                var spacing = lineLength/(eval("dataArray"+dataIndex).length);
-                return xBuffer+(i*spacing)
-            })
-            .attr("cy",yBuffer)
-            .attr("r",function(d,i){return d});
-
-        d3.select("text").text("dataset"+dataIndex);
-    });
+d3.select('#wrapper')
+	.selectAll('rect')
+	.data(myData)
+	.enter()
+	.append('rect')
+	.attr('x', function(d) {
+		return bandScale(d.day);
+	})
+  .attr('y', function(d){return height - d.value;})
+	.attr('width', bandScale.bandwidth())
+	.attr('height', function(d) {
+		return d.value;
+	});

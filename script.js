c51f40b670793b6e7fc670bd3df5e4499a1676bb
Declan Gray-Mullen <dgraymullen@umass.edu>
2022-03-24 00:19:24 +0000

    added pie!

--- script.js
+++ script.js
@@ -1,68 +1,17 @@
-// http://bl.ocks.org/alansmithy/e984477a741bc56db5a5
+var radius = 75;
 
-const svg = d3.select('svg');
-//2 different data arrays
-var dataArray1 = [30,35,45,55,70];
-var dataArray2 = [50,55,45,35,20,25,25,40];
+var svg = d3.select('svg');
+var chart = svg.append("g")
+    .attr("transform", "translate(" + 200 + "," + 200 + ")");
 
-//globals
-var dataIndex=1;
-var xBuffer=50;
-var yBuffer=150;
-var lineLength=400;
-
-
-svg.append("text")
-    .attr("x",xBuffer+(lineLength/2))
-    .attr("y",50)
-    .text("dataset"+dataIndex);
-
-//create axis line
-svg.append("line")
-    .attr("x1",xBuffer)
-    .attr("y1",yBuffer)
-    .attr("x1",xBuffer+lineLength)
-    .attr("y2",yBuffer)
-
-//create basic circles
-svg.append("g").selectAll("circle")
-    .data(eval("dataArray"+dataIndex))
-    .enter()
-    .append("circle")
-    .attr("cx",function(d,i){
-        var spacing = lineLength/(eval("dataArray"+dataIndex).length);
-        return xBuffer+(i*spacing)
-    })
-    .attr("cy",yBuffer)
-    .attr("r",function(d,i){return d});
-
-//button to swap over datasets
-d3.select("body").append("button")
-    .text("change data")
-    .on("click",function(){
-        //select new data
-        if (dataIndex==1) {
-            dataIndex=2;
-        } else   {
-            dataIndex=1;
-        }
-        //rejoin data
-        var circle = svg.select("g").selectAll("circle")
-            .data(eval("dataArray"+dataIndex));
-
-        circle.exit().remove();//remove unneeded circles
-        circle.enter().append("circle")
-            .attr("r",0);//create any new circles needed
-
-        //update all circles to new positions
-        circle.transition()
-            .duration(500)
-            .attr("cx",function(d,i){
-                var spacing = lineLength/(eval("dataArray"+dataIndex).length);
-                return xBuffer+(i*spacing)
-            })
-            .attr("cy",yBuffer)
-            .attr("r",function(d,i){return d});
-
-        d3.select("text").text("dataset"+dataIndex);
-    });
+var data = [ 5, 10, 20, 45, 6, 25 ];
+var color = d3.scaleOrdinal().domain(data).range(['red','blue','green','orange','pink']);
+var pie = d3.pie();
+var arcs = chart.selectAll("g.arc")
+  .data(pie(data))
+  .join('path')
+  .attr('d', d3.arc()
+      .innerRadius(0)
+      .outerRadius(radius)
+    )
+  .attr('fill', function(d){ return(color(d.data)) })

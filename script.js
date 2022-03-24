891cfd7fb41e7c43ad97b2f15235b608e4eb6620
Declan Gray-Mullen <dgraymullen@umass.edu>
2022-03-24 01:49:32 +0000

    added force-layout graph

--- script.js
+++ script.js
@@ -1,28 +1,68 @@
-var myData = [
-	{day : 'Mon', value: 10},
-	{day : 'Tue', value: 40},
-	{day : 'Wed', value: 30},
-	{day : 'Thu', value: 60},
-	{day : 'Fri', value: 30}
-];
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
 
-var height = 100;
+var simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(width / 2, height / 2));
 
-var bandScale = d3.scaleBand()
-	.domain(['Mon', 'Tue', 'Wed', 'Thu', 'Fri'])
-	.range([0, 300])
-	.paddingInner(0.05);
+d3.json("miserables.json", function(error, graph) {
+  if (error) throw error;
 
-d3.select('#wrapper')
-	.selectAll('rect')
-	.data(myData)
-	.enter()
-	.append('rect')
-	.attr('x', function(d) {
-		return bandScale(d.day);
-	})
-  .attr('y', function(d){return height - d.value;})
-	.attr('width', bandScale.bandwidth())
-	.attr('height', function(d) {
-		return d.value;
-	});
+  var link = svg.append("g")
+      .attr("class", "links")
+    .selectAll("line")
+    .data(graph.links)
+    .enter().append("line");
+
+  var node = svg.append("g")
+      .attr("class", "nodes")
+    .selectAll("circle")
+    .data(graph.nodes)
+    .enter().append("circle")
+      .attr("r", 2.5)
+      .call(d3.drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended));
+
+  node.append("title")
+      .text(function(d) { return d.id; });
+
+  simulation
+      .nodes(graph.nodes)
+      .on("tick", ticked);
+
+  simulation.force("link")
+      .links(graph.links);
+
+  function ticked() {
+    link
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  }
+});
+
+function dragstarted(d) {
+  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+  d.fx = d.x;
+  d.fy = d.y;
+}
+
+function dragged(d) {
+  d.fx = d3.event.x;
+  d.fy = d3.event.y;
+}
+
+function dragended(d) {
+  if (!d3.event.active) simulation.alphaTarget(0);
+  d.fx = null;
+  d.fy = null;
+}

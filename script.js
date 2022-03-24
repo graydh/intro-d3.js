0d15d025bb0291e91060701b329abee065177682
Declan Gray-Mullen <dgraymullen@umass.edu>
2022-03-24 02:07:21 +0000

    add world map classic

--- script.js
+++ script.js
@@ -1,68 +1,24 @@
-// http://bl.ocks.org/alansmithy/e984477a741bc56db5a5
+// https://gist.github.com/d3noob/82f4db23d47971c74699abb5f4bf8204
+var width = 960,
+    height = 500;
 
-const svg = d3.select('svg');
-//2 different data arrays
-var dataArray1 = [30,35,45,55,70];
-var dataArray2 = [50,55,45,35,20,25,25,40];
+var projection = d3.geoMercator();
 
-//globals
-var dataIndex=1;
-var xBuffer=50;
-var yBuffer=150;
-var lineLength=400;
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
+var path = d3.geoPath()
+    .projection(projection);
 
-svg.append("text")
-    .attr("x",xBuffer+(lineLength/2))
-    .attr("y",50)
-    .text("dataset"+dataIndex);
+var g = svg.append("g");
 
-//create axis line
-svg.append("line")
-    .attr("x1",xBuffer)
-    .attr("y1",yBuffer)
-    .attr("x1",xBuffer+lineLength)
-    .attr("y2",yBuffer)
+// load and display the World
+d3.json("world-110m2.json").then(function(topology) {
 
-//create basic circles
-svg.append("g").selectAll("circle")
-    .data(eval("dataArray"+dataIndex))
-    .enter()
-    .append("circle")
-    .attr("cx",function(d,i){
-        var spacing = lineLength/(eval("dataArray"+dataIndex).length);
-        return xBuffer+(i*spacing)
-    })
-    .attr("cy",yBuffer)
-    .attr("r",function(d,i){return d});
+    g.selectAll("path")
+       .data(topojson.feature(topology, topology.objects.countries).features)
+       .enter().append("path")
+       .attr("d", path);
 
-//button to swap over datasets
-d3.select("body").append("button")
-    .text("change data")
-    .on("click",function(){
-        //select new data
-        if (dataIndex==1) {
-            dataIndex=2;
-        } else   {
-            dataIndex=1;
-        }
-        //rejoin data
-        var circle = svg.select("g").selectAll("circle")
-            .data(eval("dataArray"+dataIndex));
-
-        circle.exit().remove();//remove unneeded circles
-        circle.enter().append("circle")
-            .attr("r",0);//create any new circles needed
-
-        //update all circles to new positions
-        circle.transition()
-            .duration(500)
-            .attr("cx",function(d,i){
-                var spacing = lineLength/(eval("dataArray"+dataIndex).length);
-                return xBuffer+(i*spacing)
-            })
-            .attr("cy",yBuffer)
-            .attr("r",function(d,i){return d});
-
-        d3.select("text").text("dataset"+dataIndex);
-    });
+});
